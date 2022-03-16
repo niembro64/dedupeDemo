72bf0ff70b4d50430219fe2a8c7a4f94479ff1bc
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-03-16 11:02:31 -0400

    init

--- dedupeDemo.js
+++ dedupeDemo.js
@@ -1,12 +1,25 @@
 // removed duplicates of characters
 // keeps only last inistance
 
-const dedupe = (str)  => {
+const dedupe = (str) => {
+  var charChecker = [];
 
+  for (var i = str.length - 1; i >= 0; i--) {
+    if (charChecker[str.charCodeAt(i)] == null) {
+      charChecker[str.charCodeAt(i)] = 1;
+    } else {
+      str = str.substring(0, i) + str.substring(i + 1, str.lenght);
+    }
+  }
+  return str;
+};
 
+s = [
+  "Snaps! crackles! pops!",
+  "Did  I shine my shoes today?",
+  "scoop dedupe!",
+  "aabbccddeeff112233  aabbcc",
+];
+for (var i = 0; i < s.length; i++) {
+  console.log(dedupe(s[i]));
 }
-
-s1 = "Snaps! crackles! pops!"
-s2 = "Did  I shine my shoes today?"
-s3 = "scoop dedupe!"
-
