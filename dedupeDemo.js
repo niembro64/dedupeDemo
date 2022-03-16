6d0596f835eeb8159ae16d4e9422b7c923f2306a
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-03-16 10:05:16 -0400

    init

---
+++ dedupeDemo.js
@@ -0,0 +1,12 @@
+// removed duplicates of characters
+// keeps only last inistance
+
+const dedupe = (str)  => {
+
+
+}
+
+s1 = "Snaps! crackles! pops!"
+s2 = "Did  I shine my shoes today?"
+s3 = "scoop dedupe!"
+
